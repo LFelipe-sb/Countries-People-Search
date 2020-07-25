e4fca82a138235bb01cbb4538351f3a9d4f21dbb
Luis Felipe <luisfelipe.sb@outlook.com>
2020-07-25 20:40:47 -0300

    Consumindo os dados da API e armazenando nos obj

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,13 +1,41 @@
 let globalCountries = [];
-let globalUsers = [];
+let globalPeople = [];
 let globalUserCountry = [];
 
-function start(){
-    console.log("teste");
+async function start(){
+    await fetchPeople();
+    await fetchCountries();
 }
 
-function fetchUsers(){}
-function fetchCountries(){}
+async function fetchPeople(){
+    const response = await fetch('http://localhost:3002/people');
+    const json = await response.json();
+
+    //Maneira sem Destructing
+    globalPeople = json.map(person => {
+        return{
+            userId: person.login.UUID,
+            userCountry: person.nat,
+            userName: person.name.first,
+            userPicture: person.picture.large
+        }
+    })
+}
+
+async function fetchCountries(){
+    const response = await fetch('http://localhost:3001/countries');
+    const json = await response.json();
+
+    //Maneira com Destructing
+    globalCountries = json.map(({name, flag, alpha2code}) => {
+        return{
+            countryName: name,
+            countryFlag: flag,
+            countryCode: alpha2code
+        }
+    });
+    
+}
 function hideSpinner(){}
 function mergeUsersAndCountries(){}
 function render(){}
