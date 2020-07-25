a86593086b779e0fc85ba546c7f1c1b82d54a7e9
Luis Felipe <luisfelipe.sb@outlook.com>
2020-07-25 19:59:10 -0300

    Guardando dados json da API externa de maneira local com Json-Server.

---
+++ Frontend/js/script.js
@@ -0,0 +1,41 @@
+let globalCountries = [];
+let globalUsers = [];
+let globalUserCountry = [];
+
+function start(){
+    console.log("teste");
+}
+
+function fetchUsers(){}
+function fetchCountries(){}
+function hideSpinner(){}
+function mergeUsersAndCountries(){}
+function render(){}
+
+start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Links da API:
+//'https://randomuser.me/api/?results=100&seed=promise';
+// 'https://restcountries.eu/rest/v2/all';
